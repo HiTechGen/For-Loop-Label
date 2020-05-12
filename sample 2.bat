a8606a42a9e3098540b13b8a7b2a4ee51f590b9a
HiTechGen <45202262+HiTechGen@users.noreply.github.com>
2020-05-12 22:17:00 +0800

    Create sample 2.bat

---
+++ sample 2.bat
@@ -0,0 +1,15 @@
+@Echo Off & Setlocal EnableDelayedExpansion
+@Title XOR test
+::For loop function
+For /L %%. In () Do (
+    ::Setting up xor labels
+    If !label! Equ 0 (
+        Set "label=1"
+        If !label! Equ 0 Echo.Hi
+    )
+    If !label! Equ 1 (
+        Echo.Stop it . . .
+        Pause >Nul
+        Exit
+    )
+)
